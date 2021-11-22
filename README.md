dd4b4c5041cb2f888bad9670c381ea8ec74fffdf
Maiu0528 <maiusutheshan@gmail.com>
2021-11-22 09:42:17 +0530

    first commit

---
+++ README.md
@@ -0,0 +1,24 @@
+# What is this?
+
+Get perfect shadows every time for the non-designer.
+
+# Instalation
+npm i shadowbost --save
+
+Then...
+
+'''
+
+import { shadowbost } from 'shadowbost;
+
+    shadowbost({
+        shadow_type: 'soft',
+        padding: false
+});
+'''
+
+## Options
+shadowbost supports 2 options, oth of which are optional:
+
+* *shadow_type* - hard | soft (Default is soft)
+* *padding* - boolean (Default is false)
---
+++ index.js
@@ -0,0 +1,23 @@
+function shadowbost(options) {
+    let components  = document.querySelectorAll('.shadowbost');
+
+    if (options.shadow_type === "hard") {
+        options.shadow_type = "0px";
+    }
+    else{
+        options.shadow_type = "15px";
+    }
+
+    components.forEach(component => {
+        component.style.boxShadow = `10px 10px ${options.shadow_type} 1px rgba(0,0,0,0.12)`;
+
+        if( options.padding) {
+            component.style.padding= "1em";
+        }
+        else {
+            component.style.padding= "0px";
+        }
+    })
+}
+
+module.exports.shadowbost = shadowbost;
